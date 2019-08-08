642b52c36dc68afec4048d1be685c2bc2526929b
kaka <jin.coolwind@qq.com>
2019-08-08 16:06:00 +0800

    leyou-common以及leyou-item的提交

---
+++ leyou-gateway/src/main/java/com/leyou/LeyouGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author: jinbang
+ * @create: 2019/8/8 15:35
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LeyouGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouGatewayApplication.class, args);
+    }
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
@@ -0,0 +1,17 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: jinbang
+ * @create: 2019/8/8 15:51
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LeyouItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouItemApplication.class);
+    }
+}
