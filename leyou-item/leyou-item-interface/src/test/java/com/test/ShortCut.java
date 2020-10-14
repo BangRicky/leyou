de8475016c753bc7d167faa5b928a1bc363e84b3
jinbang <jin.coolwind@qq.com>
2020-10-14 16:59:47 +0800

    品牌树目录查询以及查询

---
+++ leyou-item/leyou-item-interface/src/test/java/com/test/ShortCut.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import com.leyou.item.pojo.Category;
+
+/**
+ * @author jinbang
+ * @version 2019/8/16
+ */
+public class ShortCut {
+    //自动代码  Category
+    
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author jinbang
+ * @version 2019/12/19
+ */
+public interface CategoryMapper extends Mapper<Category> {
+}
