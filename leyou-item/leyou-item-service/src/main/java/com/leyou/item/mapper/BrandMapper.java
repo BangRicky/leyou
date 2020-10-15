699c2dbb47d1015bc38e6a26c81bb61b8ca9a2fc
jinbang <jin.coolwind@qq.com>
2020-10-15 14:34:29 +0800

    品牌查询后台实现-分页列表

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,12 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author jinbang
+ * @version 2020/10/15
+ */
+public interface BrandMapper extends Mapper<Brand> {
+
+}
