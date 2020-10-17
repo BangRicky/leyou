6ce16a31000cc8ac456d81dc842be6e4fbcf8fe8
jinbang <jin.coolwind@qq.com>
2020-10-17 13:13:33 +0800

    品牌新增

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -1,6 +1,8 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.pojo.Brand;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
@@ -9,4 +11,6 @@ import tk.mybatis.mapper.common.Mapper;
  */
 public interface BrandMapper extends Mapper<Brand> {
 
+    @Insert("INSERT INTO tb_category_brand (category_id, brand_id) values (#{cid}, #{bid})")
+    void insertCategoryAndBrand(@Param("cid") Long cid,@Param("bid") Long bid);
 }
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -8,6 +8,7 @@ import com.leyou.item.pojo.Brand;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -38,7 +39,7 @@ public class BrandService {
 
         // 添加排序条件
         if (StringUtils.isNotBlank(sortBy)) {
-            example.setOrderByClause(sortBy + " " + (desc ? "desc":"asc"));
+            example.setOrderByClause(sortBy + " " + (desc ? "desc" : "asc"));
         }
 
         List<Brand> brands = this.brandMapper.selectByExample(example);
@@ -46,9 +47,21 @@ public class BrandService {
         // 包装成PageInfo
         PageInfo<Brand> brandPageInfo = new PageInfo<>(brands);
         // 包装成分页结果集返回
-        return new PageResult<>(brandPageInfo.getTotal(),brandPageInfo.getList());
+        return new PageResult<>(brandPageInfo.getTotal(), brandPageInfo.getList());
 
 
+    }
+
+    @Transactional
+    public void saveBrand(Brand brand, List<Long> cids) {
+
+        // 先新增brand
+        this.brandMapper.insertSelective(brand);
+
+        // 再新增中间表
+        cids.forEach(cid -> {
+            this.brandMapper.insertCategoryAndBrand(cid, brand.getId());
+        });
 
     }
 }
